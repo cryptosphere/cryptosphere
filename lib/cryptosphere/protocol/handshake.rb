33d4b06630d90af4820e8401322e7d70deec21a4
Tony Arcieri <tony.arcieri@gmail.com>
2012-07-08 19:07:09 -0700

    Complete handshake

--- lib/cryptosphere/protocol/handshake.rb
+++ lib/cryptosphere/protocol/handshake.rb
@@ -1,32 +1,37 @@
 module Cryptosphere
   module Handshake
-    class RequestEncoder
-      def initialize(sender, recipient)
-        @sender, @recipient = sender, recipient
-      end
+    module_function
 
-      def to_signed_message
-        # FIXME: We need a different algorithm for this to be possible
-        # Sure would be nice to have some Curve25519 here
-        # message = @recipient.public_key.public_encrypt @sender.public_key
-        message = @sender.public_key
-
-        Cryptosphere.sign(@sender.private_key, message) + message
-      end
+    def encode_request(sender, recipient)
+      # TODO: encrypt sender's public key
+      # Sure would be nice to have some Curve25519 here
+      message = sender.public_key
+      Cryptosphere.sign(sender.private_key, message) + message
     end
 
-    class RequestDecoder
-      attr_reader :public_key
+    def decode_request(recipient, message)
+      bytes = PUBKEY_SIZE / 8
+      signature, message = message[0...bytes], message[bytes..-1]
 
-      def initialize(recipient, message)
-        signature = message.slice!(0, PUBKEY_SIZE / 8)
+      # FIXME: this should be encrypted :(
+      sender_key = message
+      Cryptosphere.verify!(sender_key, message, signature)
 
-        # FIXME: Use some crypto here
-        sender_key = message
-        Cryptosphere.verify!(sender_key, message, signature)
+      sender_key
+    end
 
-        @public_key = sender_key
-      end
+    def encode_response(sender, recipient, secret = Cryptosphere.random_bytes(32))
+      cipher = AsymmetricCipher.new(recipient.public_key)
+      message = cipher.public_encrypt(secret)
+      Cryptosphere.sign(sender.private_key, message) + message
+    end
+
+    def decode_response(recipient, sender, message)
+      bytes = PUBKEY_SIZE / 8
+      signature, message = message[0...bytes], message[bytes..-1]
+      Cryptosphere.verify!(sender.public_key, message, signature)
+      cipher = AsymmetricCipher.new(recipient.private_key)
+      cipher.private_decrypt(message)
     end
   end
 end
--- spec/cryptosphere/protocol/handshake_spec.rb
+++ spec/cryptosphere/protocol/handshake_spec.rb
@@ -1,16 +1,22 @@
 require 'spec_helper'
 
 describe Cryptosphere::Handshake do
-  let(:alice) { Cryptosphere::Identity.new(alice_private_key) }
-  let(:bob)   { Cryptosphere::Identity.new(bob_private_key) }
+  let(:alice)  { Cryptosphere::Identity.new(alice_private_key) }
+  let(:bob)    { Cryptosphere::Identity.new(bob_private_key) }
+  let(:secret) { "X" * 32 }
 
   it "encodes and decodes request packets" do
-    handshake = Cryptosphere::Handshake::RequestEncoder.new(alice, bob)
-    packet = handshake.to_signed_message
+    packet = Cryptosphere::Handshake.encode_request(alice, bob)
 
     # FIXME: need elliptic curve crypto here for smaller packets
     #packet.length.should be < MAX_DATAGRAM_LENGTH
-    request = Cryptosphere::Handshake::RequestDecoder.new(bob, packet)
-    request.public_key.should eq alice.public_key
+    public_key = Cryptosphere::Handshake.decode_request(bob, packet)
+    public_key.should eq alice.public_key
+  end
+
+  it "encodes and decodes response packets" do
+    packet   = Cryptosphere::Handshake.encode_response(bob, alice, secret)
+    response = Cryptosphere::Handshake.decode_response(alice, bob, packet)
+    response.should eq secret
   end
 end
