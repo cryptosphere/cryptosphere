3434fb49148611ad95ddc99a7c31935dc7329620
Tony Arcieri <tony.arcieri@gmail.com>
2012-11-30 21:25:02 -0800

    Password sphere of doom

--- lib/cryptosphere/resources/home.rb
+++ lib/cryptosphere/resources/home.rb
@@ -1,5 +1,6 @@
 class Cryptosphere::Resource::Home < Webmachine::Resource
   def to_html
-    "<html><body>Hello, world!</body></html>"
+    # Hax serve a static file!
+    File.read File.expand_path("../../assets/index.html", __FILE__)
   end
 end
