b58a0dc3cfb27d70707e8a870baa6a755a610ee0
Tony Arcieri <tony.arcieri@gmail.com>
2013-07-09 01:10:04 -0700

    Fixups for Cryptosphere::Git::Refs
    - Fixes Content-Type
    - Webmachine will auto-gzip for us
    - Gzipping was actually breaking the output
    - Match git version in response

--- lib/cryptosphere/git/refs.rb
+++ lib/cryptosphere/git/refs.rb
@@ -5,17 +5,19 @@ require 'stringio'
 module Cryptosphere
   module Git
     class Refs < Webmachine::Resource
-      # Packfile header for the HTTP-based git-receive-pack service
+      # Pack data header for the HTTP-based git-receive-pack service
       # TODO: roll this up with some other related code somewhere
       SERVICE_HEADER = "001f# service=git-receive-pack"
 
       def encodings_provided
-        # TODO: not working? o_O
-        {'gzip' => :encode_gzip}
+        {
+          'gzip'     => :encode_gzip,
+          'identity' => :encode_identity
+        }
       end
 
       def content_types_provided
-        [['application/octet-stream', :to_bytes]]
+        [['application/x-git-receive-pack-advertisement', :to_bytes]]
       end
 
       def resource_exists?
@@ -37,29 +39,17 @@ module Cryptosphere
         # support more than just empty repositories.
         response << [
           "000000880000000000000000000000000000000000000000",
-          "capabilities^{}",
+          "capabilities^{}\0",
           "report-status",
           "delete-refs",
           "side-band-64k",
           "quiet",
           "ofs-delta",
-          "agent=cryptosphere/#{VERSION}"
+          "agent=git/1.8.1.6"
         ].join(' ')
 
         response << "0000"
-        gzip response.join("\n")
-      end
-
-      # TODO: this shouldn't be necessary, it should be provited by Webmachine
-      # but it's not working for whatever reason
-      def gzip(string)
-        output = StringIO.new
-
-        gz = Zlib::GzipWriter.new(output)
-        gz.write string
-        gz.close
-
-        output.to_s
+        response.join("\n")
       end
     end
   end
