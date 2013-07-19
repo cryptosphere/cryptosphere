ec16f75189633174f264ce32f35761131a2c86f1
Tony Arcieri <tony.arcieri@gmail.com>
2013-07-18 21:47:47 -0700

    Move packfile into a fixture

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,8 @@ require 'rubygems'
 require 'bundler/setup'
 require 'cryptosphere'
 require 'webmachine/test'
+require 'pathname'
+
+def fixture(name)
+  Pathname.new(File.expand_path("../fixtures", __FILE__)).join(name.to_s).read
+end
