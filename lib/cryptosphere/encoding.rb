eb58fcd19ff1156fd98cd044f5ef2a7dcb8ead2b
Tony Arcieri <tony.arcieri@gmail.com>
2013-07-18 23:12:30 -0700

    We do not yet implement z-base-32
    I appear to have misunderstood the exact nature of Zooko's Base32, which is
    well-documented here:
    
    http://philzimmermann.com/docs/human-oriented-base-32-encoding.txt
    
    There are a number of other permutations that have been made to Base32 which are
    not correctly captured in the Cryptosphere's present implementation. Among other
    things characters which are easily confused have been removed.
    
    The specific encoding specification should be considered TBD. In the meantime we
    can at least correct the oversight and note that we are using a semi-proprietary
    version of Base32 (although not really, this style is supported by things like
    Google Authenticator)

--- lib/cryptosphere/encoding.rb
+++ lib/cryptosphere/encoding.rb
@@ -2,15 +2,16 @@ require 'base32'
 
 module Cryptosphere
   # Base32 content encoding provider. Used for all Cryptosphere URIs
-  # Uses a tweaked "Zooko-style" Base32 derivative, where lower case
-  # characters are used ubiquitously
+  # Uses a tweaked Base32 derivative, where lower case characters are
+  # used ubiquitously and no padding characters are added, similar to
+  # z-base-32 but with all possible characters present.
   module Encoding
     module_function
 
-    # Encode a string in Zooko-style Base32
+    # Encode a string in Cryptosphere-style Base32
     #
     # @param string [String] arbitrary string to be encoded
-    # @return [String] lovely, elegant "Zooko-style" Base32
+    # @return [String] lovely, elegant Cryptosphere-style Base32
     def encode(string)
       Base32.encode(string).downcase.sub(/=+$/, '')
     end
